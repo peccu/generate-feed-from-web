a797a2ccfadc1902a04f134b33facaa0ca17a4b8
peccu <peccul+github@gmail.com>
2024-06-27 10:56:22 +0000

    add debug log

--- netlify/functions/feedExtractor.ts
+++ netlify/functions/feedExtractor.ts
@@ -11,9 +11,10 @@ const handler: Handler = async (event) => {
       body: JSON.stringify({ error: 'URL parameter is required' }),
     }
   }
-
+  console.log(`Fetching ${url}`);
   try {
     const html = await fetchHtml(url)
+    console.log(`html: ${html}`);
     const feedUrls = extractFeedUrls(html, url)
 
     return {
@@ -31,9 +32,9 @@ const handler: Handler = async (event) => {
 
 function fetchHtml(url: string): Promise<string> {
   return new Promise((resolve, reject) => {
-    https.get(url, (res) => {
+    https.get(url, (res: any) => {
       let data = ''
-      res.on('data', (chunk) => data += chunk)
+      res.on('data', (chunk: string) => data += chunk)
       res.on('end', () => resolve(data))
     }).on('error', reject)
   })
@@ -42,8 +43,8 @@ function fetchHtml(url: string): Promise<string> {
 function extractFeedUrls(html: string, baseUrl: string): string[] {
   const regex = /<link[^>]*type=["'](application\/rss\+xml|application\/atom\+xml)["'][^>]*href=["']([^"']+)["'][^>]*>/gi
   const feedUrls: string[] = []
-  let match
-
+  let match: RegExpExecArray | null;
+  console.log(`matched count: ${html.match(regex)?.length}`);
   while ((match = regex.exec(html)) !== null) {
     const feedUrl = new URL(match[2], baseUrl).href
     feedUrls.push(feedUrl)
