841406364acfded839fb61108459d9314222fdd9
peccu <peccul+github@gmail.com>
2024-11-16 01:54:56 +0000

    add article parser using postlight/parser

---
+++ netlify/functions/extract-article-body.ts
@@ -0,0 +1,42 @@
+import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
+import Parser from "@postlight/parser";
+
+const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+  const params = event.queryStringParameters;
+
+  if (!params) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: "Missing query parameters" }),
+    };
+  }
+
+  const { url } = params;
+
+  if (!url) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: "Missing required parameters" }),
+    };
+  }
+
+  try {
+    const result = await Parser.parse(url);
+    console.log(result);
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/rss+xml",
+      },
+      body: result,
+    };
+  } catch (error) {
+    console.error("Error generating RSS feed:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Failed to generate RSS feed" }),
+    };
+  }
+};
+
+export { handler };
